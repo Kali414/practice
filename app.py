d5a9a3050a10b4b629b6bde7184e3cc84e8236fb
Kalicharan <kalisahoo2004@gmail.com>
2025-05-04 16:28:05 +0530

    Changes in app.py

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 print("Hello world")
 n=int(input("Enter the number"))
+
+def prime(n):
+    cnt=0
+    for x in range(1,n**0.5+1):
+        if(n%x==0):
+            cnt+=1
+
+        if(n%(n/x)==0):
+            cnt+=1
+
+    
+    if(cnt>2):
+        print(f"{n} is not a prime number")
+
+    else:
+        print(f"{n} is a prime number")
